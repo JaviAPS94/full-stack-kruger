f523601ab9181fd3472dc47028511aba76e12164
alex.pinaida <alex.pinaida@rappi.com>
2024-07-20 14:59:05 -0500

    clase 20 julio

---
+++ clase20Julio/fetch/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          throw new Error("Error en la petición");
+        }
+
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <>
+      <h1>Listado de POSTS</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
