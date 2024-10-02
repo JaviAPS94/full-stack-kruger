7d635ef8753f4836cb64cf8c8a9c69aa418f2139
alex.pinaida <alex.pinaida@rappi.com>
2024-10-01 20:31:24 -0500

    01 oct

--- clase30Sept/mongo/index.js
+++ clase30Sept/mongo/index.js
@@ -26,10 +26,70 @@ async function run() {
       views: 1000000,
     };
 
-    //await collection.insertOne(personDocument);
+    const personDocument1 = {
+      name: { first: "Alex", last: "Hamilton" },
+      birth: new Date(1950, 1, 22),
+      death: new Date(1994, 2, 8),
+      contribs: ["Ninguna"],
+      views: 100,
+    };
+
+    const personDocument2 = {
+      name: { first: "Maria", last: "Espinoza" },
+      birth: new Date(1960, 5, 15),
+      death: new Date(1998, 3, 10),
+      contribs: ["Pizarra"],
+      views: 15000,
+    };
+
+    // await collection.insertOne(personDocument1);
+    // await collection.insertOne(personDocument2);
+
+    // await collection.insertMany([personDocument1, personDocument2]);
     //Vamos a consultar info en la BDD
-    const person = await collection.findOne();
-    console.log(person);
+    // const person = await collection.findOne();
+    // console.log(person);
+    // Lo que retorna el find es un objeto llamado cursor: una referencia a los documentos que cumplen con determinada condición
+    // const people = await collection.find({}).toArray();
+    // console.log(people);
+
+    //Find with filter
+    // const peopleFiltered = await collection.find({ views: 130000 }).toArray();
+    // console.log(peopleFiltered);
+
+    //Find with filter GT -> Greater than
+    // const peopleFiltered = await collection
+    //   .find({ views: { $gt: 100000 } })
+    //   .toArray();
+    // console.log(peopleFiltered);
+
+    //Debemos buscar los documentos donde las vistas sean menores a 100000
+    // const peopleFiltered = await collection
+    //   .find({ views: { $lt: 100000 } })
+    //   .toArray();
+    // console.log(peopleFiltered);
+
+    //Debemos buscar las personas que tengan las vistas mayores a 10000 y menores a 120000
+    // const peopleFiltered = await collection
+    //   .find({ views: { $gt: 10000, $lt: 120000 } })
+    //   .toArray();
+    // console.log(peopleFiltered);
+
+    //Debemos buscar las personas que tengan las vistas mayores a 100000 y además que su fecha de nacimiento sea mayor a 1960-01-01
+    // const peopleFiltered = await collection
+    //   .find({ views: { $gt: 100000 }, birth: { $gt: new Date(1960, 1, 1) } })
+    //   .toArray();
+    // console.log(peopleFiltered);
+
+    //Vamos a buscar la persona, que tenga el nombre de Lisa y el apellido sea Mine
+    // const peopleFiltered = await collection
+    //   .find({ name: { first: "Lisa", last: "Mine" } })
+    //   .toArray();
+    // console.log(peopleFiltered);
+    const peopleFiltered = await collection
+      .find({ "name.first": "Lisa", "name.last": "Mine" })
+      .toArray();
+    console.log(peopleFiltered);
   } finally {
     await client.close();
   }
